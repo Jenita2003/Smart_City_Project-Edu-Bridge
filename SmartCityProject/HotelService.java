f001b5ae50ec44bc22dedc896587b8d4a4ac53e0
Jenita2003 <167445488+Jenita2003@users.noreply.github.com>
2025-05-04 14:50:57 +0530

    Create HotelService.java

---
+++ SmartCityProject/HotelService.java
@@ -0,0 +1,8 @@
+public class HotelService {
+    public void showHotels() {
+        System.out.println("\n--- Top Hotels ---");
+        System.out.println("1. Taj Palace");
+        System.out.println("2. The Oberoi");
+        System.out.println("3. Marriott");
+    }
+}
